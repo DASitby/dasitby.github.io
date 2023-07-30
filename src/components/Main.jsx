2c34b74082126c84faadf06b428db64854fc5f82
DASitby <davidsooley551@gmail.com>
2023-07-30 16:48:11 -0230

    add states for window text and window visibility

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,33 +1,25 @@
+'use client'
 import Image from 'next/image'
 import Link from './Link'
+import Window from './Window'
+import { useState } from 'react';
 export default function Main(){
+  const [showWindow, setShowWindow] = useState(false);
+  const [windowText, setWindowText] = useState("")
+  const handleShowWindow = () => {
+    setShowWindow(true);
+  };
+
+  const handleHideWindow = () => {
+    setShowWindow(false);
+  };
+
+
   return(
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-         You fuckin did it chap yeah:
-          <code className="font-mono font-bold"> src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        David Sooley's Portfolio Site
       </div>
-
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
@@ -38,13 +30,13 @@ export default function Main(){
           priority
         />
       </div>
-
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <Link name="Docs" href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" text="Find in-depth information about Next.js features and API."/>
-        <Link name="Learn" href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app" text="Learn about Next.js in an interactive course with&nbsp;quizzes!"/>
-        <Link name="Templates" href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" text="Explore the Next.js 13 playground."/>
-        <Link name="Deploy" href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app" text="Instantly deploy your Next.js site to a shareable URL with Vercel."/>
+        <Link name="Docs" text="Find in-depth information about Next.js features and API." setWindowText={setWindowText} handleShowWindow={handleShowWindow} handleHideWindow={handleHideWindow}/>
+        <Link name="Learn" text="Learn about Next.js in an interactive course with&nbsp;quizzes!" setWindowText={setWindowText} handleShowWindow= {handleShowWindow} handleHideWindow={handleHideWindow}/>
+        <Link name="Templates" text="Explore the Next.js 13 playground." setWindowText={setWindowText} handleShowWindow= {handleShowWindow} handleHideWindow={handleHideWindow}/>
+        <Link name="Deploy" text="Instantly deploy your Next.js site to a shareable URL with Vercel." setWindowText={setWindowText} handleShowWindow= {handleShowWindow} handleHideWindow={handleHideWindow}/>
       </div>
+      <Window isVisible={showWindow} text={windowText} handleHideWindow={handleHideWindow}/>
     </main>
   )
 }
