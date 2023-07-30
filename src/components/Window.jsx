368e6de362726f7e9c8d581d3e7fc16984cced16
DASitby <davidsooley551@gmail.com>
2023-07-30 16:47:16 -0230

    create window component

---
+++ src/components/Window.jsx
@@ -0,0 +1,16 @@
+'use client'
+export default function Window({ isVisible, handleHideWindow, text }) {
+  // Use isVisible prop to control the visibility of the window
+  return isVisible ? (
+    <div className="window-overlay">
+      <div className="window-content">
+        {/* Add your window content here */}
+        <h1>Window Content</h1>
+        <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          {text}
+        </p>
+        <button onClick={handleHideWindow}>Close Window</button>
+      </div>
+    </div>
+  ) : null;
+}
